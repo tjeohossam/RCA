b0634d7a7468a907f48e7e46b1c3aa0fe795a15e
TJEO Hossam <tjeo.hossam9@gmail.com>
2020-10-05 22:33:03 +0200

    berkati

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,39 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    pour register un user
 // @access  Public
 
-router.post('/', (req, res) => {
-  res.send('Register a User');
+router.post(
+  '/',
+  [
+    check('name', 'Please enter your name!!').not().isEmpty(),
+
+    check('email', 'Please enter a valide email!!').isEmail(),
+
+    check(
+      'password',
+      'Please enter a password with 6 or more caractere!!'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed');
+  }
+);
+router.get('/', (req, res) => {
+  res.send('hossam');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connest Database
+connectDB();
+
+//init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'welcome to the contact KeePer API ...' })
